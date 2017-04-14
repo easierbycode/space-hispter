fd0ed799e62cfe8456b03018b36ce0d58c7f76a5
dwyermt <mike@jernung.com>
2017-04-14 13:06:09 +0800

    Add move player functionality

--- app/states/HomeState.js
+++ app/states/HomeState.js
@@ -1,6 +1,34 @@
 class HomeState extends Phaser.State {
 
+  init() {
+
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    this.BULLET_SPEED = -1000;
+
+  }
+
   create() {
+
+    this.background = new globalObjects.Background(this.game);
+    this.player = new globalObjects.Player(
+      this.game,
+      this.game.world.centerX,
+      this.game.world.height - 50
+    );
+
+  }
+
+  update() {
+
+    this.player.body.velocity.x = 0;
+
+    if(this.game.input.activePointer.isDown) {
+      let direction = this.background.getClickDirection();
+      this.player.setSpeed(direction);
+    }
+
+    
 
   }
 
