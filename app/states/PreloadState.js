52ea0365ca91deb93c717ddae032c5df1296be2c
dwyermt <mike@jernung.com>
2017-04-14 12:19:01 +0800

    Add assets

--- app/states/PreloadState.js
+++ app/states/PreloadState.js
@@ -4,6 +4,21 @@ class PreloadState extends Phaser.State {
 
   preload() {
 
+    this.load.image('space', './images/space.png');
+    this.load.image('player', './images/player.png');
+    this.load.image('bullet', './images/bullet.png');
+    this.load.image('enemyParticle', './images/enemyParticle.png');
+    this.load.spritesheet('yellowEnemy', './images/yellow_enemy.png', 50, 46, 3, 1, 1);
+    this.load.spritesheet('redEnemy', './images/red_enemy.png', 50, 46, 3, 1, 1);
+    this.load.spritesheet('greenEnemy', './images/green_enemy.png', 50, 46, 3, 1, 1);
+
+    //load level data
+    //this.load.text('level1', 'assets/data/level1.json');
+    //this.load.text('level2', 'assets/data/level2.json');
+    //this.load.text('level3', 'assets/data/level3.json');
+
+    this.load.audio('orchestra', ['./audio/8bit-orchestra.mp3', 'assets/audio/8bit-orchestra.ogg']);
+
   }
 
   create() {
