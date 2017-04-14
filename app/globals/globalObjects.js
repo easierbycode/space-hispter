6ac18b3bc8249bf45f5473bd891f1bb346ebd5f3
dwyermt <mike@jernung.com>
2017-04-14 13:05:58 +0800

    Add player

--- app/globals/globalObjects.js
+++ app/globals/globalObjects.js
@@ -1,5 +1,9 @@
+import Background from '../objects/Background';
+import Player from '../objects/Player';
+
 window.globalObjects = {
-  
+  Background: Background,
+  Player: Player
 };
 
 export default globalObjects;
---
+++ app/objects/Player.js
@@ -0,0 +1,25 @@
+class Player extends Phaser.Sprite {
+
+    constructor(game, x, y, key) {
+
+        super(game, x, y, 'player');
+
+        this.PLAYER_SPEED = 200;
+
+        this.anchor.setTo(0.5);
+        this.game.physics.arcade.enable(this);
+        this.body.collideWorldBounds = true;
+
+        game.add.existing(this);
+
+    }
+
+    setSpeed(direction) {
+
+        this.body.velocity.x = direction * this.PLAYER_SPEED;
+
+    }
+
+}
+
+export default Player;
