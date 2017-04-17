b9708076c9355b43c60de9d03d1dbb17b530fe42
dwyermt <mike@jernung.com>
2017-04-17 10:30:49 +0800

    Put emitter in separate method

--- app/objects/Enemy.js
+++ app/objects/Enemy.js
@@ -41,23 +41,25 @@ class Enemy extends Phaser.Sprite {
         this.play('getHit');
 
         if(this.health <= 0) {
-            let emitter = this.game.add.emitter(
-                this.x,
-                this.y,
-                100
-            );
-            emitter.makeParticles('enemyParticle');
-            emitter.minParticleSpeed.setTo(-200, -200);
-            emitter.maxParticleSpeed.setTo(200, 200);
-            emitter.gravity = 0;
-            emitter.start(true, 500, null, 100);
-
+            this.createDeathEmitter();
         }
 
     }
 
+    createDeathEmitter() {
 
-   
+        let emitter = this.game.add.emitter(
+            this.x,
+            this.y,
+            100
+        );
+        emitter.makeParticles('enemyParticle');
+        emitter.minParticleSpeed.setTo(-200, -200);
+        emitter.maxParticleSpeed.setTo(200, 200);
+        emitter.gravity = 0;
+        emitter.start(true, 500, null, 100);
+
+    }
 
 }
 
